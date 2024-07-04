cd3ef97bca3014f4a8cdf9d52545e13d4b2a058d
邓仕昊[sx_dancehole] <sx_dancehole@canway.net>
2024-07-04 16:36:28 +0800

    ref: 优化体验

--- render.py
+++ render.py
@@ -1,7 +1,7 @@
 from jinja2 import Environment, FileSystemLoader
 import json
 
-NAME = 'cyy'    # 全局定义文件名/输入输出
+NAME = 'dsh'    # 全局定义文件名/输入输出
 
 # 加载模板环境
 env = Environment(loader=FileSystemLoader('templates'))  # 假设模板文件在'templates'目录下
@@ -17,6 +17,6 @@ output_html = template.render(data)
 # 打印或保存输出
 # print(output_html)
 # 或者保存到文件
-with open(f'./output/{NAME}.html', 'w', encoding='utf-8') as file:
+with open(f'./{NAME}.html', 'w', encoding='utf-8') as file:
     file.write(output_html)
     print("写入成功！")
